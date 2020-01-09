eeb97770f26521c26e73bc69020ba1b9271f8784
Brian Zelip <bzelip@gmail.com>
2020-01-09 05:28:27 -0500

    router.js: Add routes and export

--- src/router.js
+++ src/router.js
@@ -1,1 +1,34 @@
 import VueRouter from 'vue-router';
+
+import App from './App.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: App,
+    props: { page: 'Home' }
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: App,
+    props: { page: 'About' }
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: App,
+    props: { page: 'Contact' }
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    return savedPosition ? savedPosition : { x: 0, y: 0 };
+  }
+});
+
+export default router;
