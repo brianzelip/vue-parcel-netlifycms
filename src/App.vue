c27c1a8ccf3510be3c56c96ad2d1fd1368de0a11
Brian Zelip <bzelip@gmail.com>
2020-01-09 06:48:20 -0500

    App.vue: Update file

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Hello World!</h1>
+  <div>
+    <TheHeader></TheHeader>
+    <router-view></router-view>
+  </div>
 </template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import Home from "./layouts/Home.vue";
+import About from "./layouts/About.vue";
+import Contact from "./layouts/Contact.vue";
+
+export default {
+  props: ["page"],
+  components: {
+    TheHeader,
+    Home,
+    About,
+    Contact
+  }
+};
+</script>
